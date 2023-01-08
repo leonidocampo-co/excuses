2396ac0aa18fccd302b0b1ee6f7b0fd920c38764
Leonid ocampo <lnckocampo@yahoo.com>
2023-01-08 04:11:42 +0000

    cambios nuevos

--- src/app.js
+++ src/app.js
@@ -7,5 +7,32 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let who = ["The dog", "My grandma", "His turtle", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "the keys", "the car"];
+  let when = [
+    "before the class",
+    "right on time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying",
+  ];
+
+  let excusa = "";
+
+  let num = Math.floor(Math.random() * who.length - 1);
+  excusa = who[num] + "";
+
+  num = Math.floor(Math.random() * action.length - 1);
+  excusa = excusa + action[num];
+
+  num = Math.floor(Math.random() * what.length - 1);
+  excusa = excusa + what[num];
+
+  num = Math.floor(Math.random() * when.length - 1);
+  excusa = excusa + "";
+
+  document.getElementById("excusa").innerText = excusa;
+
+  console.log(excusa);
 };
